7ae189342a9a647bbe5f2d496b5b8069ce6cca94
Vova-cod <75933393+Vova-cod@users.noreply.github.com>
2026-05-05 02:07:02 +0200

    creat classes Transport, Bus, Train

--- main.py
+++ main.py
@@ -1,1 +1,68 @@
-print("Hello World")
+from datetime import timedelta
+
+class Transport:
+    def __init__(self, number: str, capacity: int):
+        self._number = number
+        self._capacity = capacity
+
+    @property
+    def capacity(self):
+        return self._capacity
+
+    @property
+    def number(self):
+        return self._number
+
+class Bus(Transport):
+    def __init__(self, number: str, traffic_level: int = 1):
+        super().__init__(number, capacity = 45)
+        self.traffic_level = traffic_level
+
+    @property
+    def traffic_level(self):
+        return self._traffic_level
+
+    @traffic_level.setter
+    def traffic_level(self, value: int):
+        self._traffic_level = max(1, min(10, value))
+
+    def get_info(self):
+        return(f"Bus {self._number} |"
+               f" Capacity {self._capacity} |"
+               f" TrafficLevel {self._traffic_level}/10")
+
+    def calc_delay(self):
+            delay_minut = (self._traffic_level - 1) * 2
+            return timedelta(minutes=delay_minut)
+
+class Tram(Transport):
+    def __init__(self, number: str, rail_condition: int = 10):
+        super().__init__(number, capacity = 120)
+        self.rail_condition = rail_condition
+
+    @property
+    def rail_condition(self):
+        return self._rail_condition
+
+    @rail_condition.setter
+    def rail_condition(self, value: int):
+        self._rail_condition = max(1, min(10, value))
+
+    def get_info(self):
+        return(f"Tram {self._number} |"
+                f" Capacity {self._capacity} |"
+                f" Rail Condition {self._rail_condition}/10")
+
+    def calc_delay(self):
+        delay_minut = (10 - self._rail_condition) * 3
+        return timedelta(minutes=delay_minut)
+
+vehicles = [
+    Bus("А101", traffic_level=7),
+    Tram("Т5", rail_condition=4),
+    Bus("А202", traffic_level=2),
+    ]
+
+for v in vehicles:
+    print(v.get_info())
+    print(f"  Delay: {v.calc_delay()}\n")
